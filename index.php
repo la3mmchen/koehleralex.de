53dd85656afb280ff1c69285ab5e0a3178157324
alex <alex@k3wl.net>
2015-12-18 14:27:35 +0100

    Initiale Version 1

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>koehleralex.de</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row-fluid">
+      <div class="span2">
+        <ul class="nav nav-list">
+          <li class="nav-header">koehleralex.de</li>
+        </ul>
+        <ul class="nav nav-list">
+          <li class="divider"></li>
+        </ul>
+      </div>
+    <div class="span10">
+      <div class="page-header">
+        <h1> Hej. <small>Alexander K&ouml;hler.</small></h1>
+        <p class="lead"> This is my personal web page.
+          <br/><br/>
+          <img src="alexkoehler.jpg" class="image2big" width="200px"/>
+          <img src="alexkoehler_1.jpg" class="image2big" width="93px"/>
+          <br/><br/>
+          Drop me mail at meinereiner (at) koehleralex (dot) de </p>
+      </div>
+    </div>
+  </div>
+  <div class="row-fluid">
+    <div class="span2">
+      &nbsp;
+    </div>
+    <div class="span10">
+      <div class="page-header">
+        <h1> See also. <small>My other web pages.</small></h1>
+        <div class="row-fluid marketing">
+          <div class="span6">
+            <h4>k3wl.net</h4>
+            <p>A collection of web projects.
+              <br/>
+              <small><a href="http://www.k3wl.net" target='_blank'>visit</a></small>
+            </p>
+
+            <h4>la3mmchen.de</h4>
+            <p>Some placeholder with links to all my social media accounts.
+            <br/>
+            <small><a href="http://www.la3mmchen.de" target='_blank'>visit</a></small>
+            </p>
+          </div>
+          <div class="span6">
+            <h4>lifewithbaby.de</h4>
+            <p>I started a blog some times ago to write about my life with my little daughter.
+            <br/>
+            <small><a href="http://www.lifewithbaby.de" target='_blank'>visit</a></small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+  <script src="http://code.jquery.com/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
